0c9171c1c37dcfa71b4a36ce4dd6b7a22732edb4
Erik Ensminger <erik.ensminger@mail.utoronto.ca>
2021-12-01 22:25:12 -0500

    added the sidebar and mainPanel to the shiny app

--- inst/shiny-scripts/app.R
+++ inst/shiny-scripts/app.R
@@ -1,7 +1,74 @@
 ui <- fluidPage(
-  #... code
+  # App title ----
+  titlePanel(tags$h1(tags$b("FastRPA:"),"Plant Root Measurement filtering, analysis and interpretation")),
+
+  # Sidebar layout with input and output definitions ----
+  sidebarLayout(
+
+    # Sidebar panel for inputs ----
+    sidebarPanel(
+
+      tags$p("The shiny app allows user to choose a input file, the app
+             will plot the root measurements with respect to treatment type and gene lines."),
+
+      tags$p("Input file MUST be of .csv format"),
+
+      # input
+      fileInput("file1", "Choose FASTA file", accept = c("text/csv", "text/comma-separated-values,text/plain",".csv")),
+
+      # actionButton
+      actionButton(inputId = "button1", label = "Start!"),
+
+      br(),
+
+    ), # End of side pannel
+
+    mainPanel(
+
+      # Output: Tabet
+      tabsetPanel(type = "tabs",
+                  tabPanel("Plot",
+                           plotOutput("plot"), verbatimTextOutput('textOut')),
+                  tabPanel("Data table", dataTableOutput('textOut1'))
+      )
+    )
+  )
 )
+
+
+
+#req(input$file1)
+
+# Define server logic for random distribution app ----
 server <- function(input, output) {
-  #... code
+
+  #press the start button lead to running this reaction
+  processInput <- eventReactive(eventExpr = input$button1, {
+
+    req(input$file1)
+    inFile <- input$file1
+    printer <- createRootBarG(inFile$datapath)
+    #check file existence
+  })
+
+  #result plot output
+  output$plot <- renderPlot ({
+    createRootBarG(processInput())
+  })
+
+  # #text output
+  # output$textOut <- renderPrint({
+  #   if (! is.null(processInput()))
+  #     processInput()$printer
+  # })
+
+  # #Data frame output
+  # output$textOut1 <- renderDataTable({
+  #   if (! is.null(processInput()))
+  #     processInput()
+  # })
+
 }
 shinyApp(ui = ui, server = server)
+
+
